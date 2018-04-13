17af46830879f08af9a2815c32dc6883414d0b8a
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-13 19:58:42 +0200

    Create photos migrations

---
+++ migrations/1523641903977_create_photos_table.js
@@ -0,0 +1,23 @@
+exports.up = (pgm) => {
+	pgm.createTable("photos", 
+	{
+		user_id:
+		{
+			type:"int",
+			notNull: true, 
+			primaryKey: true, 
+			references: "users", 
+			onDelete: "cascade",
+			onUpdate: "cascade" 
+		},
+		link: {type: "text", notNull: true, primaryKey: true},
+	},
+	{
+		ifNotExists: true
+	});
+};
+
+exports.down = (pgm) => {
+	pgm.dropTable("photos", {ifExists: true})
+
+};
