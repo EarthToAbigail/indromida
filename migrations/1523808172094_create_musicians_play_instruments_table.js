b684732a1f79feea6921b81fcd11a566d4aec703
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-15 18:04:39 +0200

    Create musicians table

---
+++ migrations/1523808172094_create_musicians_play_instruments_table.js
@@ -0,0 +1,27 @@
+exports.up = (pgm) => {
+	pgm.createTable("musicians_play_instruments", 
+	{
+		user_id:
+		{
+			type:"int",
+			notNull: true, 
+			primaryKey: true, 
+			references: "users", 
+			onDelete: "cascade",
+			onUpdate: "cascade" 
+		},
+		instrument:
+		{
+			type: "varchar(100)",
+			notNull: true,
+			primaryKey: true
+		}
+	},
+	{
+		ifNotExists: true
+	});
+};
+
+exports.down = (pgm) => {
+	pgm.dropTable("musicians_play_instruments", {ifExists: true});
+};
