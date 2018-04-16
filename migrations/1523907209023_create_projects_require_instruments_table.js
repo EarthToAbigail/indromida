8b6463bdf15dbe09d5a372c31d09d660ca11542b
Abdullah Emad <abdullahem1997@hotmail.com>
2018-04-16 21:36:28 +0200

    Create project related tables migrations

---
+++ migrations/1523907209023_create_projects_require_instruments_table.js
@@ -0,0 +1,27 @@
+exports.up = (pgm) => {
+	pgm.createTable("projects_require_instruments", 
+	{
+		pid:
+		{
+			type:"int",
+			notNull: true, 
+			primaryKey: true, 
+			references: "projects", 
+			onDelete: "cascade",
+			onUpdate: "cascade" 
+		},
+		instrument:
+		{
+			type: "varchar(100)",
+			notNull: true,
+			primaryKey: true
+		}
+	},
+	{
+		ifNotExists: true
+	});
+};
+
+exports.down = (pgm) => {
+	pgm.dropTable("musicians_play_instruments", {ifExists: true});
+};
