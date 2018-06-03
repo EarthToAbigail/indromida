d41cfda63edb1c57031710904c3ac9fca48564c7
EarthToAbigail <earth_to_abigail@protonmail.com>
2018-06-03 21:27:22 +0300

    connect db to server

--- db/db_connect.js
+++ db/db_connect.js
@@ -1,5 +1,32 @@
-// const { Pool } = require('pg');
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: true
-// });
+const { Pool } = require('pg');
+
+const url = require('url');
+require('env2')('.env');
+
+let DATABASE_URL = process.env.DATABASE_URL;
+
+if (process.env.NODE_ENV === 'test') {
+  DATABASE_URL = process.env.TEST_DATABASE_URL;
+}
+
+if (!DATABASE_URL) {
+  throw new Error('ENV variable DATABASE_URL was NOT SET!!');
+}
+
+const pool = new Pool({
+  connectionString: DATABASE_URL
+});
+
+const params = url.parse(DATABASE_URL);
+
+const options = {
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  max: process.env.DB_MAX_CONNECTIONS || 2
+  // user: params.auth.split(':')[0],
+  // password: params.auth.split(':')[1]
+};
+
+options.ssl = options.host !== 'localhost';
+module.exports = new Pool(options);
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const dbConnect = require('../db/db_connect');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -33,19 +34,38 @@ if (process.env.NODE_ENV === 'production') {
     app.get('/', (req, res) => {
         res.send({ env: "PROD", page: "HOME" });
     });
-    // Any other route leads to 404.
-    app.get('*', (req, res) => {
-        res.status(404).send("Sorry, cant't find what you are looking for...");
+
+    app.get('/db', (req, res) => {
+      dbConnect.query('SELECT * FROM users', (err, success) => {
+        if (err) {
+          res.send(err);
+        } else if (success.rowCount === 0) {
+          res.send('table empty');
+        }
+        res.send(success.rows);
+      });
     });
-    // Ask Express to eventually serve the index.html if the route is not
-    // recognized.
-    // app.use(express.static('client/build'));
-    // app.get('*', (req, res) => {
-    //     res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
-    // });
+
+    //Ask Express to eventually serve the index.html if the route is not
+    //recognized.
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+    });
 } else {
     app.get('/', (req, res) => {
        res.send({ env: "DEV", page: "HOME" });
+    });
+
+    app.get('/db', (req, res) => {
+      dbConnect.query('SELECT * FROM users', (err, success) => {
+        if (err) {
+          res.send(err);
+        } else if (success.rowCount === 0) {
+          res.send('table empty');
+        }
+        res.send(success.rows);
+      });
     });
 
     // Any other route leads to 404.
